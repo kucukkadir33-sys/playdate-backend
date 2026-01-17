2fe0ec2611d3f7ce9d561aeb9dbcb2bab9aa0d02
kucukkadir33-sys <kucuk.kadir33@gmail.com>
2026-01-17 16:46:34 +0300

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,38 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ktor)
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.7"
+    application
 }
 
 group = "com.playdate"
 version = "0.0.1"
 
 application {
-    mainClass = "io.ktor.server.netty.EngineMain"
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenCentral()
 }
 
 dependencies {
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.websockets)
-    implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
-    implementation(libs.ktor.server.config.yaml)
-    testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test.junit)
+
+    // Ktor core
+    implementation("io.ktor:ktor-server-core-jvm:2.3.7")
+
+    // Netty engine
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.7")
+
+    // WebSocket
+    implementation("io.ktor:ktor-server-websockets-jvm:2.3.7")
+
+    // YAML config (opsiyonel ama sorun çıkarmaz)
+    implementation("io.ktor:ktor-server-config-yaml:2.3.7")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+
+    // Test
+    testImplementation("io.ktor:ktor-server-test-host-jvm:2.3.7")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.22")
 }
