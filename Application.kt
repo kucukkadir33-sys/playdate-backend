060384867f6927b9284ef3da751c13f91ef6b450
kucukkadir33-sys <kucuk.kadir33@gmail.com>
2026-01-17 16:40:31 +0300

    Application.kt

--- Application.kt
+++ Application.kt
@@ -4,6 +4,15 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
+fun main() {
+    embeddedServer(
+        Netty,
+        host = "0.0.0.0",
+        port = System.getenv("PORT")?.toInt() ?: 8080
+    ) {
+        module()
+    }.start(wait = true)
+}
 
 fun Application.module() {
     configureSockets()
